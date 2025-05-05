c01ce1b74ec4b9cd8a3b9f3c96d0acf262aa4585
Marut K <me@rezigned.com>
2025-05-05 23:16:09 +0700

    chore: clean up code [skip ci]

--- src/go/flake.nix
+++ src/go/flake.nix
@@ -11,7 +11,7 @@
       systems,
     }:
     let
-      version = "1.24";
+      name = "go";
       eachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
@@ -19,23 +19,30 @@
         system:
         let
           pkgs = import nixpkgs { inherit system; };
-          gopkg = pkgs.go_1_24;
           arch = builtins.elemAt (builtins.split "-" system) 0;
           os = builtins.elemAt (builtins.split "-" system) 1;
         in
         {
-          go = pkgs.stdenv.mkDerivation {
-            pname = "hello-go";
-            version = version;
-            src = ./.;
-            nativeBuildInputs = [ gopkg ];
+          ${name} = pkgs.stdenv.mkDerivation rec {
+            pname = "hello-${name}";
+            version = "1.0";
+            src =
+              with pkgs.lib.fileset;
+              toSource {
+                root = ../.;
+                fileset = union ./. ../check.sh;
+              };
+            sourceRoot = "${src.name}/${name}";
+            nativeBuildInputs = [ pkgs.go_1_24 ];
+            doCheck = true;
             buildPhase = ''
               export CGO_ENABLED=0
               export GOCACHE=$(mktemp -d)
 
-              go build -ldflags="-s -w" -o hello-go main.go
+              go build -ldflags="-s -w" -o hello main.go
             '';
-            installPhase = "mkdir -p $out/bin; mv hello-go $out/bin/hello-go";
+            checkPhase = "../check.sh";
+            installPhase = "mkdir -p $out/bin; mv hello $out/bin/hello-${name}";
           };
         }
       );

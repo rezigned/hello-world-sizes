705d7b9fec71cc0846b1c1daa4d50314650ada4f
Marut Khumtong <me@rezigned.com>
2025-05-11 00:29:13 +0700

    Fix Odin issue on arm64

--- src/odin/flake.nix
+++ src/odin/flake.nix
@@ -18,8 +18,17 @@
       packages = eachSystem (
         system:
         let
-          pkgs = import nixpkgs { inherit system; };
           arch = builtins.elemAt (builtins.split "-" system) 0;
+          pkgs = import nixpkgs { inherit system; };
+          musl = pkgs.pkgsMusl.musl;
+
+          # Extra inputs
+          inputs =
+            {
+              x86_64 = [ pkgs.nasm ];
+              aarch64 = [ pkgs.llvmPackages.lld ];
+            }
+            ."${arch}" or (throw "Unsupported arch: ${arch}");
         in
         {
           ${name} = pkgs.stdenv.mkDerivation rec {
@@ -34,8 +43,7 @@
             sourceRoot = "${src.name}/${name}";
             nativeBuildInputs = [
               pkgs.odin
-              pkgs.nasm
-            ];
+            ] ++ inputs;
             doCheck = true;
             buildPhase = ''
               # See https://gaultier.github.io/blog/odin_and_musl.html
@@ -45,14 +53,25 @@
                   ;;
 
                 aarch64-linux)
-                  # The -no-crt flag is not yet supported on ARM64.
-                  odin build main.odin -file -o:size -disable-assert -default-to-nil-allocator -out:hello
+                  # The -no-crt flag is not yet supported on ARM64. So, we need to build an object file and link it manually.
+                  odin build main.odin -file -target=linux_arm64 -build-mode=object
+
+                  # Manually link the object file into a statically-linked binary
+                  ld.lld \
+                    ${musl}/lib/crt1.o \
+                    ${musl}/lib/crti.o \
+                    ${musl}/lib/libc.a \
+                    ${musl}/lib/libm.a \
+                    ${musl}/lib/crtn.o \
+                    --sysroot=${musl} \
+                    main.o -o hello -static
                   ;;
 
                 *)
                   echo "Unsupported system: ${system}"
                   exit 1
                   ;;
+              esac
             '';
             checkPhase = "../check.sh";
             installPhase = "mkdir -p $out/bin; mv hello $out/bin/hello-${name}";

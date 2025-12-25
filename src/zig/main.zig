9a1f9c480c56e8fcb65b31ae17c9d37aaead918a
Marut Khumtong <me@rezigned.com>
2025-12-25 15:49:42 +0700

    chore: update deps

--- src/zig/main.zig
+++ src/zig/main.zig
@@ -3,9 +3,9 @@ const std = @import("std");
 
 pub fn main() !void {
     // zig < 0.15
-    const outw = std.io.getStdOut().writer();
-    try outw.print("Hello, world!\n", .{});
+    // const outw = std.io.getStdOut().writer();
+    // try outw.print("Hello, world!\n", .{});
 
     // zig >= 0.15
-    // try std.fs.File.stdout().writeAll("Hello, world!\n");
+    try std.fs.File.stdout().writeAll("Hello, world!\n");
 }
